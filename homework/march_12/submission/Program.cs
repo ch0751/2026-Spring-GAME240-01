bd7eea7b2fefd41519368b3ecd00ee72a3e40f04
ch0751 <leznik@chapman.edu>
2026-03-12 22:45:48 -0700

    March12thAssignment

---
+++ homework/march_12/submission/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = { 42, 7, 5, 98, 21 };
+
+int smallest = numbers[0];
+int largest = numbers[0];
+int i = 1;
+while (i < 5)
+{
+    if (numbers[i] < smallest)
+    {
+        smallest = numbers[i];
+    }
+    if (numbers[i] > largest)
+    {
+        largest = numbers[i];
+    }
+
+    i++;
+}
+Console.WriteLine("The largest number is " + largest + ". The smallest number is " + smallest + ".");
